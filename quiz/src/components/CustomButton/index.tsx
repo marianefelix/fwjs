625e5e98d51f9919eee0f4392aab9ab21ea92a37
Mariane Felix <marianefelix59@gmail.com>
2022-04-09 12:29:12 -0300

    Add comportamento dinâmico a tab

--- quiz/src/components/CustomButton/index.tsx
+++ quiz/src/components/CustomButton/index.tsx
@@ -2,13 +2,22 @@ import './styles.css';
 
 interface TabProps {
     title: string;
+    selected: boolean; 
     onClick: () => void;
 }
 
 export const CustomButton = (props: TabProps) => {
+    const getState = () => {
+        if (props.selected) {
+            return ' selected';
+        }
+
+        return '';
+    };
+
     return (
         <button
-            className="custom-button"
+            className={`custom-button${getState()}`}
             type="button" 
             title={props.title} 
             onClick={props.onClick}
--- quiz/src/components/Tab/index.tsx
+++ quiz/src/components/Tab/index.tsx
@@ -1,19 +1,57 @@
+import { useState } from "react";
 import { CustomButton } from "../CustomButton";
 import { DescriptionContainer } from "../DescriptionContainer";
 import { Enunciation } from "../Enunciation";
 
 import './styles.css';
 
+const tabList = [
+    {
+        "title": "Tab 1",
+        "description": "Texto 1",
+    },
+    {
+        "title": "Tab 2",
+        "description": "Texto 2",
+    },
+    {
+        "title": "Tab 3",
+        "description": "Texto 3",
+    },
+    {
+        "title": "Tab 4",
+        "description": "Texto 4",
+    },
+];
+
 export const Tab = () => {
+    const [currentlySelectedTab, setCurrentlySelectedTab] = useState(0);
+    
+    const tabOnClick = (tabIndex: number) => {
+        setCurrentlySelectedTab(tabIndex)
+    };
+
+    const isTabSelected = (tabIndex: number) => {
+        if (tabIndex === currentlySelectedTab) {
+            return true;
+        }
+
+        return false;
+    };
+
     return (
         <div className="tab-container">
-            <CustomButton title="Tab 1" onClick={() => {}} />
-            <CustomButton title="Tab 2" onClick={() => {}} />
-            <CustomButton title="Tab 3" onClick={() => {}} />
-            <CustomButton title="Tab 4" onClick={() => {}} />
+            {tabList.map((tab, index) => (
+                <CustomButton 
+                    key={tab.description} 
+                    title={tab.title}
+                    selected={isTabSelected(index)}
+                    onClick={() => tabOnClick(index)} 
+                />
+            ))}
 
             <DescriptionContainer>
-                <Enunciation text="Texto teste" />
+                <Enunciation text={tabList[currentlySelectedTab].description} />
             </DescriptionContainer>
         </div>
     );
