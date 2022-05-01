ec0396130c715345a25d3a40e4b7aaf7abe14d79
Mariane Felix <marianefelix59@gmail.com>
2022-05-01 14:47:13 -0300

    Cria componente de enunciado

---
+++ quiz/src/components/Enunciation/Enunciation.svelte
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    export let text: string;
+</script>
+
+
+<div class="description-container">
+    <h2 class="enunciation-text">
+        {text}
+    </h2>
+</div>
+
+<style scoped src="./styles.css"></style>
